ac73b0e307ed668d75c6b067486cde2d3e484cde
egelchie <98990580+egelchie@users.noreply.github.com>
2025-02-19 16:07:25 -0500

    Added responsive design, mix-blend-mode effect, and API fetch

---
+++ script.js
@@ -0,0 +1,4 @@
+fetch('https://catfact.ninja/fact')
+    .then(response => response.json())
+    .then(data => document.getElementById('fact').innerText = data.fact)
+    .catch(error => console.error('Error fetching cat fact:', error));
